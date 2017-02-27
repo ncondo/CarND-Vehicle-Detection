ebe749003f0a71b619dae3ddcd55191a0077933c
Nick <nicholas.condo@gmail.com>
2017-02-27 16:12:50 -0500

    Draw windows on test images

--- detect_cars.py
+++ detect_cars.py
@@ -7,6 +7,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import glob
 
 
 def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, feature_vec=True):
@@ -347,6 +348,32 @@ if __name__=='__main__':
     print(round(time.time()-t, 2), 'Seconds to train SVC...')
     # Log test accuracy
     print('Test Accuracy of SVC:', round(svc.score(X_test, y_test), 4))
+
+    test_images = glob.glob('./test_images/test*.jpg')
+    images = []
+    titles = []
+    y_start_stop = [400, 656]
+    overlap = 0.5
+    for img_src in test_images:
+        t1 = time.time()
+        img = mpimg.imread(img_src)
+        draw_img = np.copy(img)
+        img = img.astype(np.float32)
+        print(np.min(img), np.max(img))
+
+        windows = slide_window(img, x_start_stop=[None, None], y_start_stop=y_start_stop,
+                                xy_window=(128, 128), xy_overlap=(overlap, overlap))
+        hot_windows = search_windows(img, windows, svc, X_scaler, color_space=color_space,
+                                spatial_size=spatial_size, hist_bins=hist_bins,
+                                orient=orient, pix_per_cell=pix_per_cell,
+                                cell_per_block=cell_per_block, hog_channel=hog_channel,
+                                spatial_feat=spatial_feat, hist_feat=hist_feat, hog_feat=hog_feat)
+        window_img = draw_boxes(draw_img, hot_windows, color=(0, 0, 255), thick=6)
+        images.append(window_img)
+        titles.append('')
+        print(time.time()-t1, 'seconds to process one image searching', len(windows), 'windows')
+    fig = plt.figure(figsize=(12,18), dpi=300)
+    visualize(fig, 5, 2, images, titles)
 
 
 
