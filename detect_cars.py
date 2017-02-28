d0047fb8a5f80db84e150aefd5b23d3ed83c8e9e
Nick <nicholas.condo@gmail.com>
2017-02-28 02:15:13 -0500

    Explicitly pass scale parameter to find_cars func

--- detect_cars.py
+++ detect_cars.py
@@ -374,7 +374,7 @@ def draw_labeled_bboxes(img, labels):
 
 def process_image(img):
 
-    out_img, heat_map = find_cars(img, scale)
+    out_img, heat_map = find_cars(img, 1.5)
     heat_map = apply_threshold(heat_map, 1)
     labels = label(heat_map)
     # Draw bounding boxes on a copy of the image
