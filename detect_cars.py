36f417a4ddfc5f4a607520943849322cd2165015
Nick <nicholas.condo@gmail.com>
2017-02-28 23:47:37 -0500

    Apply heatmap threshold on averaged heatmaps

--- detect_cars.py
+++ detect_cars.py
@@ -130,7 +130,7 @@ def process_image(img):
         heatmap_sum -= old_heatmap
         heatmap_sum = np.clip(heatmap_sum,0.0,1000000.00)
     heatmap_avg = np.divide(heatmap_sum, len(heatmaps))
-
+    heatmap_avg = apply_threshold(heatmap_avg, 10)
     labels = label(heatmap_avg)
     #labels = label(heat_map)
     # Draw bounding boxes on a copy of the image
